0a9e5e9a8d9caaad5daadcf5de6ffa0b6a47e518
HL <hoanglinh171038@gmail.com>
2021-02-02 15:09:45 +0700

    6.pagination

--- src/component/Checkout.js
+++ src/component/Checkout.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 
 function Checkout({cartItems}) {
     return (
-        <div className="w-100 text-center" style={{"justify-content":"center"}}>
+        <div >
             <span className="text-primary font-root-title text-center font-weight-bold" style={{"font-size":"200 px"}}>Total:
             </span> {cartItems && cartItems.reduce((a,c) => a+c.price *c.count,0)}
             <span>VND</span>
--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -5,40 +5,48 @@ import Filter from './Filter'
 import {connect} from 'react-redux'
 import {fetchProducts} from './redux/Product/ProductActions'
 import {addToCart} from './redux/Cart/CartActions'
+import PropTypes from 'prop-types';
+
+ProductList.defaultProps = {
+    products:[]
+}
+ProductList.propTypes = {
+    products:PropTypes.array
+}
 function ProductList({products,fetchProducts,addToCart,cartItems}) {
+    
     //call API product
     useEffect(() => {
        fetchProducts()
     }, []);
-    
-    return <div style={{"position":"absolute",top:"7%" }} className="row">
-    <div className="col-md-2">
-        <Filter />
+    const [page,setPage] = useState(1);
+    const [numOfPage,setNumOfPage] = useState(3)
+   const  handdleChangePage = (paginationPage) =>{
+        setPage(paginationPage)
+    }
+   
+    const pagi = [];
+        for(let i=0;i<products.length/numOfPage;i++){
+           const btnPagination = <button onClick={() =>handdleChangePage(i+1)}>{i+1}</button>
+          pagi.push(btnPagination)
+        }
+    return <div>
+        <div style={{"position":"absolute",top:"7%" }} className="row">
+            <div className="col-md-2">
+                <Filter />
+            </div>
+            <ProductListWarp className="col-md-8">
+            {   products.slice((page-1)*numOfPage,numOfPage*page).map(product =>{
+                return <Product title={product.title} image={product.image} addToCart={() =>addToCart(cartItems,product)}/>
+            })}
+            </ProductListWarp>
+            
+            </div>
+            <div style={{position: "absolute", bottom: "5%", left: "24%"}}>{pagi}</div>
     </div>
-    <ProductListWarp className="col-md-8">
-     {   products &&  products.map(product =>{
-        return <Product title={product.title} image={product.image} addToCart={() =>addToCart(cartItems,product)}/>
-    })}
-    </ProductListWarp>
-    </div>
-    // return (
-    //     // <ProductListWarp>
-    //     //     this is {productData.error}
-    //     //     <Product />
-    //     // </ProductListWarp>
-    // )
+ 
 }
 
-// const mapStateToProps =(state) =>{
-//     return {
-//         products:state.products.items
-//     }
-// }
-// const mapDispatchToProps = (dispatch) =>{
-//     return {
-//         fetchProducts:()=>dispatch(fetchProducts())
-//     }
-// }
 export default connect((state) =>({products:state.products.filteredItems,cartItems:state.cart.cartItems}),{fetchProducts,addToCart})(ProductList)
 
 const ProductListWarp = styled.div`
